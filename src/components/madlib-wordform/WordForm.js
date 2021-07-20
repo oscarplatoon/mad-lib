e171d949e770839052ff3379315b56a082fe4fd4
kylewgunter <kylewgunter@gmail.com>
2021-07-20 16:48:15 -0500

    kyle and reno wip

--- src/components/madlib-wordform/WordForm.js
+++ src/components/madlib-wordform/WordForm.js
@@ -5,9 +5,21 @@ class WordForm extends Component {
   onInputChange(key, index, event) {
     this.props.onInputChange(key, event.currentTarget.value, index)
   }
+  
+  inputForm = () => {
+    let output = []
+    for(let i = 0; i < this.props.words.length; i++){
+      let word = this.props.words[i]
+      output.push(<div><input key={word.label} type='text' onChange= {event => this.props.onInputChange(word.label, event.currentTarget.value, i)} placeholder={word.label}></input></div>)
+    }
+    return output
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        {this.inputForm()}
+      </div>
     )
   }
 }
