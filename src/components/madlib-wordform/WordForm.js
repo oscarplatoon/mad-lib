83e4491a836a793d99bbd3aa6d63b13c759a5792
AlexanderS68 <80540025+AlexanderS68@users.noreply.github.com>
2021-07-20 20:27:51 -0400

    initial start

--- src/components/madlib-wordform/WordForm.js
+++ src/components/madlib-wordform/WordForm.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react'
-import './WordForm.css'
+import React, { Component } from "react";
+import "./WordForm.css";
 
 class WordForm extends Component {
-  onInputChange(key, index, event) {
-    this.props.onInputChange(key, event.currentTarget.value, index)
+
+  UserInput() {
+    let output = []
+
+    for (let i = 0; i < this.props.words.length; i++) {
+      let word = this.props.words[i]
+      output.push (
+        <div><input onChange={event => this.props.onInputChange(word.key, event.target.value, i)} type='text' key={word.key} placeholder={word.label}></input></div>
+      )
+    }
+    return output
   }
+
   render() {
+    console.log(this.props.words);
     return (
-      <div></div>
-    )
+      <div>
+        { this.UserInput() }
+      </div>
+    );
   }
 }
 
-export default WordForm
+export default WordForm;
